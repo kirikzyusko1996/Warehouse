5dad362d6412c2e7c803d8ff4d330573c65f98eb
colinforzeal <colinforzeal@yandex.ru>
2017-04-17 22:43:16 +0300

    improved warehouse customer entity

--- Database/src/main/java/entity/WarehouseCustomerCompany.java
+++ Database/src/main/java/entity/WarehouseCustomerCompany.java
@@ -1,12 +1,10 @@
 package entity;
 
 import javax.persistence.*;
-import java.io.Serializable;
 
 @Entity
 @Table(name = "warehouse_customer_company")
-public class WarehouseCustomerCompany implements Serializable{
-    private static final long serialVersionUID = 1L;
+public class WarehouseCustomerCompany{
     private Long idWarehouseCustomerCompany;
     private String name;
 
@@ -28,26 +26,5 @@ public class WarehouseCustomerCompany implements Serializable{
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        WarehouseCustomerCompany that = (WarehouseCustomerCompany) o;
-
-        if (idWarehouseCustomerCompany != null ? !idWarehouseCustomerCompany.equals(that.idWarehouseCustomerCompany) : that.idWarehouseCustomerCompany != null)
-            return false;
-        if (name != null ? !name.equals(that.name) : that.name != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = idWarehouseCustomerCompany != null ? idWarehouseCustomerCompany.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        return result;
     }
 }
