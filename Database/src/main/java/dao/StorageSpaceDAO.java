e484706d1c72948b2b69724dcf57e6f23a1938de
Alexey Maleyko <alexeymaleyko@gmail.com>
2017-04-19 22:50:06 +0400

    Create StorageSpaceDAO

---
+++ Database/src/main/java/dao/StorageSpaceDAO.java
@@ -0,0 +1,12 @@
+package dao;
+
+import entity.StorageSpace;
+
+/**
+ * Created by Alexey on 19.04.2017.
+ */
+public class StorageSpaceDAO  extends DAO<StorageSpace>{
+    public StorageSpaceDAO() {
+        super(StorageSpace.class);
+    }
+}
