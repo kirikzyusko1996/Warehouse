4b2e9cc03e9f4f82f6f8be0707f3caad4c49fb57
Alexey Maleyko <alexeymaleyko@gmail.com>
2017-04-19 22:49:32 +0400

    Create StorageCellDAO

---
+++ Database/src/main/java/dao/StorageCellDAO.java
@@ -0,0 +1,12 @@
+package dao;
+
+import entity.StorageCell;
+
+/**
+ * Created by Alexey on 19.04.2017.
+ */
+public class StorageCellDAO extends DAO<StorageCell> {
+    public StorageCellDAO() {
+        super(StorageCell.class);
+    }
+}
