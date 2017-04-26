4bd41826eee65680966605e21df036fb073972b7
colinforzeal <colinforzeal@yandex.ru>
2017-04-26 22:02:00 +0300

    added equals() and hashCode() methods

--- Database/src/main/java/com/itechart/warehouse/entity/Invoice.java
+++ Database/src/main/java/com/itechart/warehouse/entity/Invoice.java
@@ -186,4 +186,64 @@ public class Invoice {
     public void setGoodsEntryCountUnit(Unit goodsEntryCountUnit) {
         this.goodsEntryCountUnit = goodsEntryCountUnit;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Invoice invoice = (Invoice) o;
+
+        if (id != null ? !id.equals(invoice.id) : invoice.id != null) return false;
+        if (number != null ? !number.equals(invoice.number) : invoice.number != null) return false;
+        if (issueDate != null ? !issueDate.equals(invoice.issueDate) : invoice.issueDate != null) return false;
+        if (transportNumber != null ? !transportNumber.equals(invoice.transportNumber) : invoice.transportNumber != null)
+            return false;
+        if (transportName != null ? !transportName.equals(invoice.transportName) : invoice.transportName != null)
+            return false;
+        if (goodsQuantity != null ? !goodsQuantity.equals(invoice.goodsQuantity) : invoice.goodsQuantity != null)
+            return false;
+        if (goodsEntryCount != null ? !goodsEntryCount.equals(invoice.goodsEntryCount) : invoice.goodsEntryCount != null)
+            return false;
+        if (description != null ? !description.equals(invoice.description) : invoice.description != null) return false;
+        if (transportCompany != null ? !transportCompany.equals(invoice.transportCompany) : invoice.transportCompany != null)
+            return false;
+        if (warehouseCompany != null ? !warehouseCompany.equals(invoice.warehouseCompany) : invoice.warehouseCompany != null)
+            return false;
+        if (supplierCompany != null ? !supplierCompany.equals(invoice.supplierCompany) : invoice.supplierCompany != null)
+            return false;
+        if (receiverCompany != null ? !receiverCompany.equals(invoice.receiverCompany) : invoice.receiverCompany != null)
+            return false;
+        if (driver != null ? !driver.equals(invoice.driver) : invoice.driver != null) return false;
+        if (goodsQuantityUnit != null ? !goodsQuantityUnit.equals(invoice.goodsQuantityUnit) : invoice.goodsQuantityUnit != null)
+            return false;
+        if (goodsEntryCountUnit != null ? !goodsEntryCountUnit.equals(invoice.goodsEntryCountUnit) : invoice.goodsEntryCountUnit != null)
+            return false;
+        if (incomingGoods != null ? !incomingGoods.equals(invoice.incomingGoods) : invoice.incomingGoods != null)
+            return false;
+        return outgoingGoods != null ? outgoingGoods.equals(invoice.outgoingGoods) : invoice.outgoingGoods == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (number != null ? number.hashCode() : 0);
+        result = 31 * result + (issueDate != null ? issueDate.hashCode() : 0);
+        result = 31 * result + (transportNumber != null ? transportNumber.hashCode() : 0);
+        result = 31 * result + (transportName != null ? transportName.hashCode() : 0);
+        result = 31 * result + (goodsQuantity != null ? goodsQuantity.hashCode() : 0);
+        result = 31 * result + (goodsEntryCount != null ? goodsEntryCount.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (transportCompany != null ? transportCompany.hashCode() : 0);
+        result = 31 * result + (warehouseCompany != null ? warehouseCompany.hashCode() : 0);
+        result = 31 * result + (supplierCompany != null ? supplierCompany.hashCode() : 0);
+        result = 31 * result + (receiverCompany != null ? receiverCompany.hashCode() : 0);
+        result = 31 * result + (driver != null ? driver.hashCode() : 0);
+        result = 31 * result + (goodsQuantityUnit != null ? goodsQuantityUnit.hashCode() : 0);
+        result = 31 * result + (goodsEntryCountUnit != null ? goodsEntryCountUnit.hashCode() : 0);
+        result = 31 * result + (incomingGoods != null ? incomingGoods.hashCode() : 0);
+        result = 31 * result + (outgoingGoods != null ? outgoingGoods.hashCode() : 0);
+        return result;
+    }
 }
