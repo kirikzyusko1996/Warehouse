da2e97f49cbc450da483166293ea3bb18635bd22
colinforzeal <colinforzeal@yandex.ru>
2017-05-07 13:31:19 +0300

    657033: added method for retrieving companies that used system for particular period; tested id

--- Webservice/src/main/java/com/itechart/warehouse/service/impl/WarehouseCompanyServiceImpl.java
+++ Webservice/src/main/java/com/itechart/warehouse/service/impl/WarehouseCompanyServiceImpl.java
@@ -1,24 +1,32 @@
 package com.itechart.warehouse.service.impl;
 
 import com.itechart.warehouse.dao.WarehouseCompanyDAO;
+import com.itechart.warehouse.dao.WarehouseCompanyStatusDAO;
 import com.itechart.warehouse.dao.WarehouseDAO;
 import com.itechart.warehouse.dao.exception.GenericDAOException;
 import com.itechart.warehouse.entity.TransportCompany;
 import com.itechart.warehouse.entity.WarehouseCompany;
+import com.itechart.warehouse.entity.WarehouseCompanyStatus;
 import com.itechart.warehouse.service.exception.DataAccessException;
 import com.itechart.warehouse.service.exception.IllegalParametersException;
 import com.itechart.warehouse.service.exception.ResourceNotFoundException;
 import com.itechart.warehouse.service.services.WarehouseCompanyService;
 import org.apache.commons.lang3.math.NumberUtils;
+import org.hibernate.criterion.Conjunction;
 import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Created by Lenovo on 25.04.2017.
@@ -26,11 +34,17 @@ import java.util.Optional;
 @Service
 public class WarehouseCompanyServiceImpl implements WarehouseCompanyService {
     private WarehouseCompanyDAO warehouseCompanyDAO;
+    private WarehouseCompanyStatusDAO statusDAO;
     private Logger logger = LoggerFactory.getLogger(WarehouseCompanyServiceImpl.class);
 
     @Autowired
     public void setWarehouseCompanyDAO(WarehouseCompanyDAO warehouseCompanyDAO) {
         this.warehouseCompanyDAO = warehouseCompanyDAO;
+    }
+
+    @Autowired
+    public void setStatusDAO(WarehouseCompanyStatusDAO dao) {
+        this.statusDAO = dao;
     }
 
     @Override
@@ -50,7 +64,38 @@ public class WarehouseCompanyServiceImpl implements WarehouseCompanyService {
 
     @Override
     @Transactional(readOnly = true)
-    public WarehouseCompany findWarehouseCompanyById(Long id) throws DataAccessException{
+    public Set<WarehouseCompany> findAllThatUsedForPeriod(Date startDate, Date dueDate)
+            throws DataAccessException, IllegalParametersException {
+        logger.info("Find all warehouse companies, that used system from {} to {}", startDate, dueDate);
+
+        if (dueDate.after(today())) {
+            logger.error("Due date cannot be in the future");
+            throw new IllegalParametersException("Due date cannot be in the future");
+        }
+
+        if (startDate.after(dueDate)) {
+            logger.error("Start date cannot be in the after due date");
+            throw new IllegalParametersException("Start date cannot be in the after due date");
+        }
+
+        Set<WarehouseCompany> companies;
+        try {
+            List<WarehouseCompanyStatus> statuses = retrieveStatusesForPeriod(startDate, dueDate);
+            companies = statuses.stream()
+                    .map(WarehouseCompanyStatus::getWarehouseCompany)
+                    .collect(Collectors.toSet());
+
+        } catch (GenericDAOException e) {
+            logger.error("Error during searching for warehouse companies that used system for some period", e.getMessage());
+            throw new DataAccessException(e);
+        }
+
+        return companies;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public WarehouseCompany findWarehouseCompanyById(Long id) throws DataAccessException {
         logger.info("Find warehouse by id: {}", id);
         if (id == null) return null;
         WarehouseCompany warehouseCompany = null;
@@ -136,5 +181,20 @@ public class WarehouseCompanyServiceImpl implements WarehouseCompanyService {
             logger.error("Error while determine if WarehouseCompany with id: {} exists", e.getMessage());
             throw new DataAccessException(e.getCause());
         }
+    }
+
+    private List<WarehouseCompanyStatus> retrieveStatusesForPeriod(Date startDate, Date dueDate) throws GenericDAOException {
+        DetachedCriteria criteria = DetachedCriteria.forClass(WarehouseCompanyStatus.class);
+        Conjunction and = Restrictions.conjunction();
+        and.add(Restrictions.eq("status", Boolean.TRUE));
+        and.add(Restrictions.le("startDate", dueDate));
+        and.add(Restrictions.ge("dueDate", startDate));
+        criteria.add(and);
+
+        return statusDAO.findAll(criteria, -1, -1);
+    }
+
+    private Date today() {
+        return new Date(new java.util.Date().getTime());
     }
 }
