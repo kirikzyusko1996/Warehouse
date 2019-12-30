14292f1e58a55ab28c586955f54eec6bcaa8f32c
kirikzyusko1996 <zyusko.kirik@gmail.com>
2019-12-31 01:15:35 +0300

    [DSM-04] - use only CPU

--- data-seed/merge-series.py
+++ data-seed/merge-series.py
@@ -1,17 +1,12 @@
 import pandas as pd
+import random
 
 df = pd.read_csv("series-transformed.csv")
-df2 = pd.read_csv("series-transformed.csv")
 
-df2['input'] = df2['input'] + 1000000
-df2['output'] = df2['output'] + 1000000
-
-df['location_1'] = 1
-df['location_2'] = 0
-
-df2['location_1'] = 0
-df2['location_2'] = 1
-
-df = df.append(df2, ignore_index=True)
+for i in range(5):
+    df2 = pd.read_csv("series-transformed.csv")
+    df2['input'] = df2['input'] + random.randint(0, 2000)
+    df2['output'] = df2['output'] + random.randint(0, 2000)
+    df = df.append(df2, ignore_index=True)
 
 df.to_csv('series-transformed-2.csv', encoding='utf-8', index=False)
--- data-seed/nn-keras.py
+++ data-seed/nn-keras.py
@@ -1,5 +1,7 @@
-#import os
+import os
 #os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
 from pandas import read_csv
 from keras.models import Sequential
@@ -19,14 +21,13 @@ import time
 from numpy.random import seed
 seed(1)
 
-import tensorflow
-tensorflow.random.set_seed(2)
+import tensorflow as tf
 
 from tensorflow.python.client import device_lib
 print(device_lib.list_local_devices())
 
 # load dataset
-dataframe = read_csv("series-transformed.csv")
+dataframe = read_csv("series-transformed-2.csv")
 dataset = dataframe.values
 
 X = dataset[:, 2:77]
@@ -47,60 +48,60 @@ X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
 input_size = X.shape[1]
+with tf.device('/cpu:0'):
+    model = Sequential()
+    model.add(Dense(int(input_size * 1.5), input_dim=input_size, kernel_initializer='normal', kernel_regularizer=regularizers.l2(0.01), activation='relu', name='hidden_1'))
+    #model.add(Dropout(0.4, name='dropout_1'))
+    model.add(Dense(input_size, kernel_initializer='normal', activation='relu', kernel_regularizer=regularizers.l2(0.01), name='hidden_2'))
+    #model.add(Dropout(0.4, name='dropout_2'))
+    #model.add(Dense(int(input_size * 0.5), kernel_initializer='normal', activation='relu', kernel_regularizer=regularizers.l2(0.01), name='hidden_3'))
+    #model.add(Dropout(0.5, name='dropout_3'))
+    model.add(Dense(1, kernel_initializer='normal', kernel_regularizer=regularizers.l2(0.01), name='output_layer'))
 
-model = Sequential()
-model.add(Dense(int(input_size * 1.5), input_dim=input_size, kernel_initializer='normal', kernel_regularizer=regularizers.l2(0.01), activation='relu', name='hidden_1'))
-model.add(Dropout(0.4, name='dropout_1'))
-model.add(Dense(input_size, kernel_initializer='normal', activation='relu', kernel_regularizer=regularizers.l2(0.01), name='hidden_2'))
-model.add(Dropout(0.4, name='dropout_2'))
-#model.add(Dense(int(input_size * 0.5), kernel_initializer='normal', activation='relu', kernel_regularizer=regularizers.l2(0.01), name='hidden_3'))
-#model.add(Dropout(0.5, name='dropout_3'))
-model.add(Dense(1, kernel_initializer='normal', kernel_regularizer=regularizers.l2(0.01), name='output_layer'))
+    # Compile model
+    model.compile(loss='mse', optimizer='adam')
 
-# Compile model
-model.compile(loss='mse', optimizer='adam')
+    print(model.summary())
 
-print(model.summary())
+    history = None
+    start_time = time.time()
+    for train_index, test_index in tscv.split(X_train):
+        print(len(train_index))
+        Xx_train, Xx_test = X_train[train_index], X_train[test_index]
+        yy_train, yy_test = y_train[train_index], y_train[test_index]
+        history = model.fit(Xx_train, yy_train,
+                  epochs=2,
+                  verbose=0,
+                  validation_data=(Xx_test, yy_test))
 
-history = None
-start_time = time.time()
-for train_index, test_index in tscv.split(X_train):
-    # print(len(train_index))
-    Xx_train, Xx_test = X_train[train_index], X_train[test_index]
-    yy_train, yy_test = y_train[train_index], y_train[test_index]
-    history = model.fit(Xx_train, yy_train,
-              epochs=50,
-              verbose=0,
-              validation_data=(Xx_test, yy_test))
+    print("--- %s seconds ---" % (time.time() - start_time))
+    loss_values = history.history['loss']
+    epochs = range(1, len(loss_values)+1)
 
-print("--- %s seconds ---" % (time.time() - start_time))
-loss_values = history.history['loss']
-epochs = range(1, len(loss_values)+1)
+    score = model.evaluate(X_test, y_test, verbose=0)
 
-score = model.evaluate(X_test, y_test, verbose=0)
+    # 163418
+    x_input = np.array(scaler.transform([[0,251651,330914,426493,328758,287274,398647,367155,320362,316503,283214,335058,327631,223894,306459,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0]]))
+    yhat = model.predict(x_input, verbose=0)
+    print(yhat)
 
-# 163418
-x_input = np.array(scaler.transform([[0,251651,330914,426493,328758,287274,398647,367155,320362,316503,283214,335058,327631,223894,306459,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0]]))
-yhat = model.predict(x_input, verbose=0)
-print(yhat)
+    x_input = np.array(scaler.transform(X))
+    yhat = model.predict(x_input, verbose=0)
 
-x_input = np.array(scaler.transform(X))
-yhat = model.predict(x_input, verbose=0)
+    sum = 0
+    index = []
+    for i in range(len(yhat)):
+        percentage = (yhat[i][0]-y[i]) / yhat[i][0] * 100
+        # print(f'Predicted: {yhat[i][0]}, Real: {y[i]}, error: {yhat[i][0]-y[i]}, percentage: {percentage}')
+        sum += abs(percentage)
+        index.append(i)
 
-sum = 0
-index = []
-for i in range(len(yhat)):
-    percentage = (yhat[i][0]-y[i]) / yhat[i][0] * 100
-    # print(f'Predicted: {yhat[i][0]}, Real: {y[i]}, error: {yhat[i][0]-y[i]}, percentage: {percentage}')
-    sum += abs(percentage)
-    index.append(i)
+    print(f'Average error: {sum / len(yhat)}')
 
-print(f'Average error: {sum / len(yhat)}')
+    plt.plot(index, y, color='green')
+    plt.plot(index, yhat, color='red', alpha=0.5)
 
-plt.plot(index, y, color='green')
-plt.plot(index, yhat, color='red', alpha=0.5)
-
-plt.show()
+    plt.show()
 
 # 194687
 # 203000.00 - 2000 epoch/normalized
