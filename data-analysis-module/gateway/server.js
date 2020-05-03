d2cc6b421b7824ea034012bb1432a40149c82530
kiryl.ziusko <kiryl.ziusko@external.wirecard.com>
2020-05-03 20:50:25 +0300

    [WM-004] - CORS issue on gateway resolved

--- data-analysis-module/gateway/server.js
+++ data-analysis-module/gateway/server.js
@@ -7,6 +7,25 @@ const host = '0.0.0.0';
 
 // app
 const app = express();
+
+app.use((req, res, next) => {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, credentials');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use('/', (req, res, next) => {
     if (req.url.includes('forecast')) {
         proxy.web(req, res, {
