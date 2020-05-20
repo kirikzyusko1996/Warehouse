7d9fc815b31f14dec84945d9d0702c11e834b0b2
kiryl.ziusko <kiryl.ziusko@external.wirecard.com>
2020-05-20 22:05:49 +0300

    [WM-005] - python mock server

--- data-analysis-module/app/app.py
+++ data-analysis-module/app/app.py
@@ -1,6 +1,8 @@
 from typing import List, Dict
 from flask import Flask
+import mysql.connector
 import json
+import numpy as np
 
 app = Flask(__name__)
 
@@ -9,14 +11,14 @@ def favorite_colors() -> List[Dict]:
     config = {
         'user': 'root',
         'password': 'root',
-        'host': 'db',
+        'host': 'localhost',
         'port': '3306',
-        'database': 'knights'
+        'database': 'warehouse'
     }
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor()
-    cursor.execute('SELECT * FROM favorite_colors')
-    results = [{name: color} for (name, color) in cursor]
+    cursor.execute('SELECT * FROM role')
+    results = [{id_role: name} for (id_role, name) in cursor]
     cursor.close()
     connection.close()
 
@@ -25,6 +27,7 @@ def favorite_colors() -> List[Dict]:
 
 @app.route('/web/web/forecast/series')
 def series() -> str:
+    # warehouse_id
     return json.dumps({
         'real': [650, 470, 557, 489, 743, 312, 0, 683, 511, 569, 472, 774, 324, 0],
         'expected': [649, 472, 557, 487, 745, 315, 0, 687, 513, 567, 474, 771, 324, 0, 621, 455, 561, 472, 711, 329, 0],
@@ -34,7 +37,36 @@ def series() -> str:
 
 @app.route('/web/web/forecast/constructor')
 def constructor() -> str:
-    return json.dumps({'favorite_colors': ['red', 'blue', 'green']})
+    # warehouse_id
+    values = [789, 572, 667, 387, 545, 815, 0, 547, 763, 667, 521, 671, 981, 0, 561, 321, 687, 713, 1015, 415, 0]
+    return json.dumps({
+        'expected': values,
+        'total': int(np.sum(values)),
+    })
+
+
+@app.route('/web/web/forecast/constructor/initial-values')
+def initial_values() -> str:
+    # warehouse_id
+    return json.dumps({
+        'day1': 650,
+        'day2': 470,
+        'day3': 557,
+        'day4': 489,
+        'day5': 743,
+        'day6': 312,
+        'day7': 0,
+        'day8': 683,
+        'day9': 511,
+        'day10': 569,
+        'day11': 472,
+        'day12': 774,
+        'day13': 324,
+        'day14': 0,
+        'lat': 53.8868861,
+        'lng': 27.542942,
+        'dailyPrice': 45,
+    })
 
 
 @app.route('/web/web/forecast/tracker')
