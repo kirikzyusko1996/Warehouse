8befb6d3c03c59e4f9bafee090f5c9c32f604a41
kiryl.ziusko <kiryl.ziusko@external.wirecard.com>
2020-06-18 23:05:47 +0300

    [WM-005] - real working NN

--- data-analysis-module/app/app.py
+++ data-analysis-module/app/app.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from datetime import datetime, timedelta
 from typing import List, Dict
 from flask import Flask, request
@@ -6,7 +5,40 @@ import mysql.connector
 import json
 import numpy as np
 from sklearn.metrics import mean_absolute_error
+from joblib import load
 
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras import regularizers
+
+# sklearn - 0.22.2.post1, tensorflow - 2.2.0, keras - 2.3.1
+
+import keras.backend.tensorflow_backend as tb
+tb._SYMBOLIC_SCOPE.value = True
+
+model = Sequential()
+
+model.add(Dense(int(1774), input_dim=380, kernel_initializer='normal', kernel_regularizer=regularizers.l2(0.001), activation='relu', name='hidden_1'))
+model.add(BatchNormalization())
+
+model.add(Dense(int(1183), kernel_initializer='normal', activation='relu', kernel_regularizer=regularizers.l2(0.001), name='hidden_2'))
+model.add(BatchNormalization())
+model.add(Dense(int(1183), kernel_initializer='normal', activation='relu', kernel_regularizer=regularizers.l2(0.001), name='hidden_21'))
+model.add(BatchNormalization())
+
+model.add(Dense(int(591), kernel_initializer='normal', activation='relu', kernel_regularizer=regularizers.l2(0.001), name='hidden_3'))
+model.add(BatchNormalization())
+model.add(Dense(int(591), kernel_initializer='normal', activation='relu', kernel_regularizer=regularizers.l2(0.001), name='hidden_31'))
+model.add(BatchNormalization())
+
+model.add(Dense(1, kernel_initializer='normal', kernel_regularizer=regularizers.l2(0.001), name='output_layer'))
+
+# Compile model
+model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+
+model.load_weights('model-warehouse-stock-prediction.h5')
 
 app = Flask(__name__)
 
@@ -18,6 +50,17 @@ config = {
     'database': 'warehouse'
 }
 connection = mysql.connector.connect(**config)
+
+
+def to_one_hot(value, ranges):
+    one_hot = []
+    for i in range(ranges):  # KMeans was trained on 300 clusters
+        one_hot.append(0)
+
+    one_hot[value] = 1
+
+    return one_hot
+
 
 def favorite_colors() -> List[Dict]:
     config = {
@@ -42,6 +85,7 @@ def series() -> str:
     id_warehouse = request.args.get('idWarehouse')
     cursor = connection.cursor()
 
+    # ############### LAST 14 DAYS ###############
     date = datetime.today() - timedelta(days=28)  # last 14 as real + 14 for prediction "real"
 
     # TODO: remove SQL-injection attack
@@ -67,12 +111,64 @@ def series() -> str:
         except: # key does not exist
             last_trend.append(0)
 
+    real = last_trend[-14:]
+
+    # ############### Clustering ###############
+    cursor.execute("""
+        SELECT warehouse.x as lng, warehouse.y as ltd, AVG(price_list.daily_price) as daily_price FROM warehouse
+        INNER JOIN warehouse_company ON warehouse.id_warehouse_company = warehouse_company.id_warehouse_company
+        INNER JOIN price_list ON warehouse_company.id_warehouse_company = price_list.id_warehouse_company
+        WHERE warehouse.id_warehouse = @warehouse_id
+        GROUP BY warehouse.id_warehouse
+    """.replace('@warehouse_id', id_warehouse))
+
+    results = [(lng, ltd, daily_price) for (lng, ltd, daily_price) in cursor]
+    (lng, ltd, daily_price) = results[0]
+
     cursor.close()
 
-    real = last_trend[-14:]
-    # TODO: import Neural network and make calculation for 21 day
-    expected = [649, 472, 557, 487, 745, 315, 0, 687, 513, 567, 474, 771, 324, 0, 621, 455, 561, 472, 711, 329, 0]
-    # warehouse_id
+    k_means_clustering = load('clustering.joblib')
+    cluster = k_means_clustering.predict([[lng, ltd]])[0]
+
+    geo_data = to_one_hot(cluster, 300)
+
+    predictions = []
+
+    data_for_predictions_last_14_days = []
+    # predict last 14 days
+    for i in range(14):
+        # ############### DATA BUILDER ###############
+        date = datetime.today() - timedelta(days=14 - i)
+        one_hot_weekday = to_one_hot(date.weekday(), 7)
+        one_hot_week_in_year = to_one_hot(date.isocalendar()[1], 52)
+        store_type = [0, 0, 0, 0]  # mock for now
+        assortment = [0, 0, 0]  # mock for now
+
+        last_14_sales = []
+        for j in range(14):
+            last_14_sales.append(last_trend[i+j])
+
+        # sales_14
+        raw_data = one_hot_weekday + one_hot_week_in_year + store_type + assortment + geo_data + last_14_sales
+
+        data_for_predictions_last_14_days.append(raw_data)
+
+    # ############### Scaling ###############
+    scaler = load('scaler.pkl')
+    scaled_data = scaler.transform(data_for_predictions_last_14_days)
+
+    # ############### Neural Network ###############
+    data = np.array(scaled_data)
+
+    predictions = model.predict(data)
+
+    pred = []
+    for i in range(14):
+        pred.append(predictions[i][0])
+
+    # TODO: calcuate by NN next 7 days
+    expected = pred + [621, 455, 561, 472, 711, 329, 0]
+
     return json.dumps({
         'real': real,
         'expected': expected,
@@ -162,4 +258,4 @@ def tracker() -> str:
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True, threaded=False)
